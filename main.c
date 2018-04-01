8341c6e2bed8b7f80c748e0f6b46f22e78a8fbea
Chinmay Chandak <chandakchinmay@gmail.com>
2018-04-01 21:24:47 +0530

    done with basic(and initial nad not working properly) level design
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

--- main.c
+++ main.c
@@ -1,5 +1,87 @@
-#include <graphics.h>
 #include <stdio.h>
+#include <graphics.h>
+
+typedef struct player {
+	unsigned char orentation; // E(ast), W(est), N(orth), S(outh)
+	unsigned short x;
+	unsigned short y;
+	unsigned short bombs;
+} p1, p2;
+
+typedef struct tile {
+	unsigned char type;
+	// B: Blank
+	// O: Obastacle
+	// D: Destroyable
+
+	unsigned short x;
+	unsigned short y;
+} tiles[17][17];
+
+unsigned short tileSize = 26;
+unsigned short tileSizeHalf = 13;
+unsigned short offset = 0;
+
+void setup () {
+	unsigned int color;
+	unsigned short i, j;
+	unsigned short top, bottom, left, right;
+
+	char levelDesign[17][17] = {
+		{'O',   'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',   'O'},
+
+
+		{'O',   'B', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'B',   'O'},
+		{'O',   'B', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'B',   'O'},
+
+		{'O',   'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D',   'O'},
+		{'O',   'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B',   'O'},
+		{'O',   'O', 'D', 'B', 'O', 'D', 'B', 'O', 'D', 'B', 'O', 'D', 'B', 'O', 'D', 'B',   'O'},
+		{'O',   'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B',   'O'},
+		{'O',   'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D',   'O'},
+
+		{'O',   'O', 'O', 'O', 'O', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'O', 'O', 'O',   'O'},
+
+		{'O',   'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D',   'O'},
+		{'O',   'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B',   'O'},
+		{'O',   'O', 'D', 'B', 'O', 'D', 'B', 'O', 'D', 'B', 'O', 'D', 'B', 'O', 'D', 'B',   'O'},
+		{'O',   'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B',   'O'},
+		{'O',   'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D',   'O'},
+
+		{'O',   'B', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'D', 'O', 'B',   'O'},
+		{'O',   'B', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'D', 'B', 'B',   'O'},
+
+
+		{'O',   'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',   'O'}
+	};
+
+	for (i=0; i<17; i++) {
+		for (j=0; j<17; j++) {
+			// set fill style and boundry color
+			if (levelDesign[i][j] == 'O') {
+				color = WHITE;
+			} else if (levelDesign[i][j] == 'B') {
+				color = BLACK;
+			} else if (levelDesign[i][j] == 'D') {
+				color = DARKGRAY;
+			}
+
+			setcolor(color);
+			setfillstyle(1, color);
+
+			// lets calc the coords
+			top = offset + (i)*tileSize;
+			bottom = offset + (i+1)*tileSize;
+
+			left = offset + (j)*tileSize;
+			right = offset + (j+1)*tileSize;
+
+			// lets make a rectangle
+			rectangle(top, left, bottom, right);
+			floodfill(top+1, left+1, color);
+		}
+	}
+}
 
 void main () {
 	int gd = DETECT, gm, flag = 0;
@@ -7,9 +89,14 @@ void main () {
 	char ch;
 
 	clrscr();
+
 	initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
+
+	// lets setup the level
+	setup();
+
 	while (1) {
-		cleardevice();
+		//cleardevice();
 		if (kbhit()) {
 			ch = getch();
 			//printf("%d", ch);
@@ -31,8 +118,8 @@ void main () {
 				x += 10;
 				break;
 		}
-		setcolor(RED);
-		rectangle(x,getmaxy()-20, x+30, getmaxy()-10);
+		//setcolor(RED);
+		//rectangle(x,getmaxy()-20, x+30, getmaxy()-10);
 		//delay(10);
 	}
 	closegraph();
